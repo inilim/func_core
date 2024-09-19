40a2cb051d2e7c206503f83a6f1daf07b87494a5
Dmitry <40324321+inilim@users.noreply.github.com>
2024-09-19 22:44:33 +0300

    ref

---
+++ src/FuncCore.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Inilim\FuncCore;
+
+use Inilim\LazyMethod\LazyMethodAbstract;
+
+class FuncCore extends LazyMethodAbstract
+{
+    protected const NAMESPACE   = 'Inilim\FuncCore\Method';
+    protected const PATH_TO_DIR = __DIR__ . '/Method';
+}
---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Inilim\Dump\Dump;
+
+
+Dump::init();
+
+
+
+de($res);
+// de(preg_quote('xmlrpc.beeps'));
